13ba4a4f156d16d9d42a0a678b5cc52b553a1363
jezpo <77103678+jezpo@users.noreply.github.com>
2022-11-25 15:19:41 -0400

    change to styles to prototype

--- Modules/Bienestarejemplo/Resources/views/pages/panel.blade.php
+++ Modules/Bienestarejemplo/Resources/views/pages/panel.blade.php
@@ -23,10 +23,105 @@
     <!-- end page-header -->
     <!-- begin row -->
     <div class="row">
-        @php
-        echo "hola mundo";
+{{--@php
+        echo "hola mundo";/// aqui va el cuerpo del formulario
         @endphp
+    --}}
+    <div class="panel panel-inverse" data-sortable-id="table-basic-1">
+						<!-- begin panel-heading -->
+						<div class="panel-heading ui-sortable-handle">
+							<h4 class="panel-title">Default Table</h4>
+							<div class="panel-heading-btn">
+								<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand"><i class="fa fa-expand"></i></a>
+								<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success" data-click="panel-reload"><i class="fa fa-redo"></i></a>
+								<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse"><i class="fa fa-minus"></i></a>
+								<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-danger" data-click="panel-remove"><i class="fa fa-times"></i></a>
+							</div>
+						</div>
+						<!-- end panel-heading -->
+						<!-- begin panel-body -->
+						<div class="panel-body">
+              <div>
+                <h3 >Recepcion De Documentos</h3>
+                <h5>UserName - Trabajadora Social - Fecha</h5>
+              </div>
+							<!-- begin table-responsive -->
+							<div class="table-responsive">
+								<table class="table m-b-0">
+									<thead>
+										<tr>
+											<th>RU</th>
+											<th>Ci</th>
+											<th>Nombres</th>
+                      <th>Carrera</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>1</td>
+											<td>Nicky Almera</td>
+											<td>nicky@hotmail.com</td>
+                      <td>carrera 1</td>
+										</tr>
+										<tr>
+											<td>2</td>
+											<td>Edmund Wong</td>
+											<td>edmund@yahoo.com</td>
+                      <td>carrera 2</td>
+										</tr>
+										<tr>
+											<td>3</td>
+											<td>Harvinder Singh</td>
+											<td>harvinder@gmail.com</td>
+                      <td>carrera 3</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+              <div>
+              
+              <button type="button" class="btn btn-yellow" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Nueva Recepcion</button>
+              
+              <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Nueva Recepcion</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label">Recipient:</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">Message:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Send message</button>
+      </div>
     </div>
+  </div>
+</div>
+
+              <button type="button" class="btn btn-warning">Imprimir</button>
+              </div>
+							<!-- end table-responsive -->
+						</div>
+					</div>
+  </div>
+</div>
+  </div>
+</div>
+
+
     <!-- end row -->
 @endsection
 
